46c8a9669584dff65cbd2059b34ad6bc803739f1
ZacharyNWilliams <98299163+ZacharyNWilliams@users.noreply.github.com>
2022-10-17 18:11:28 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let name1 = "Jerry Rice";
-let name2 = "Deion Sanders";
+let name2 = "Deion Sanderss";
 let name3 = "Patrick Willis";
 
 if(name1.length > name2.length && name1.length > name3.length){
@@ -11,13 +11,24 @@ else if(name2.length > name1.length && name2.length > name3.length){
     console.log(`${name2} has the longest name`);
 }
 
+//name 2 = name 1 name 2!= 3
+else if(name2.length === name1.length && name2.length != name3.length){
+    console.log(`${name1} and ${name2} are the same`);
+}
+
+//name1 = name3 name1 != name2
+else if(name1.length === name3.length && name1.length != name2.length){
+    console.log(`${name2} and ${name3} are the same`);
+}
+//name2= name3 name2 != name1
+else if(name2.length === name3.length && name2.length != name1.length){
+    console.log(`${name2} and ${name3} are the same`);
+}
 
 
 else if(name3.length > name1.length && name3.length > name2.length){
     console.log(`${name3} has the longest name`)
 }
-
-
 
 else{
     console.log(`All three names, ${name1}, ${name2}, ${name3} are the same length`)
